20632d70ea2c84b54ac5e542f10264bb6a7edfd2
Andrey Zhivotov <res_a@mail.ru>
2022-02-22 00:28:13 +0300

    TrafficLights

--- TrafficLights/ViewController.swift
+++ TrafficLights/ViewController.swift
@@ -8,12 +8,57 @@
 import UIKit
 
 class ViewController: UIViewController {
-
+    
+    @IBOutlet var redLightsView: UIView!
+    @IBOutlet var yellowLightsView: UIView!
+    @IBOutlet var greenLightsView: UIView!
+    
+    @IBOutlet var startButton: UIButton!
+    
+    private var currentColor = CurrentColor.red
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        
+        redLightsView.alpha = 0.3
+        yellowLightsView.alpha = 0.3
+        greenLightsView.alpha = 0.3
+        
+        redLightsView.layer.cornerRadius = 55
+        yellowLightsView.layer.cornerRadius = 55
+        greenLightsView.layer.cornerRadius = 55
+        
+        startButton.layer.cornerRadius = 10
+        
     }
-
-
+    
+    enum CurrentColor {
+        case red
+        case yellow
+        case green
+    }
+    
+    @IBAction func startButtonPressed() {
+       
+        if startButton.isEnabled {
+            startButton.setTitle("NEXT", for: .normal)
+        }
+        
+        switch currentColor {
+        case .red:
+            greenLightsView.alpha = 0.3
+            redLightsView.alpha = 1
+            currentColor = .yellow
+        case .yellow:
+            redLightsView.alpha = 0.3
+            yellowLightsView.alpha = 1
+            currentColor = .green
+        case .green:
+            yellowLightsView.alpha = 0.3
+            greenLightsView.alpha = 1
+            currentColor = .red
+        }
+        
+    }
+    
 }
-
